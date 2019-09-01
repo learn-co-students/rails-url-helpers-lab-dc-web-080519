7281143642d3e423a4622c576e8fb6ac74babadb
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-01 15:11:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# bg = Student.create(first_name:"b",last_name:"g")
